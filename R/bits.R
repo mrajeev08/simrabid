4d60e13eb853937a5474382e89c02708ee80954d
Malavika Rajeev <mrajeev08@gmail.com>
2020-10-07 18:27:08 -0700

    updating param distributions

---
+++ R/bits.R
@@ -0,0 +1,50 @@
+# ------------------------------------------------------------------------------
+#' utils
+# ------------------------------------------------------------------------------
+# think about how flexible you want this to
+# all functions should have n & param list
+# pass parameters for each into param list in sim.IBM
+# if you're fitting these then you want to be able to pass params easily in
+# the args of the function (I think this works well? Just need unique names?)
+
+# if you want to change this then just need to change the param list & the functions accordingly
+
+# secondary function (should take from param list (n & params))
+secondaries <- function(n, params = c(R0 = 1.2, k = 1, max = 100)) {
+  secondaries <- rnbinom(n, size = params$k, mu = params$R0)
+
+  # return constrained!
+  return(ifelse(secondaries > max, max, secondaries))
+}
+
+# dispersal function
+# defaults to Townsend et al. 2013 plos ntds
+dispersal_dist <- function(n, params = c(disp_shape = 1.46,
+                                         disp_scale = 16.1)) {
+
+  rgamma(n, shape = params$disp_shape, scale = params$disp_scale)
+
+}
+
+# defaults to Townsend et al. 2013 plos ntds
+# could also use an empirical sample of generation times per rebecca
+gen_time <- function(n, params = c(inc_shape = 1.46, inc_scale = 16.1),
+                          ...) {
+
+  rgamma(n, shape = params$inc_shape, scale = params$inc_scale)
+
+}
+
+# generation time floored to the time step
+t_infectious <- function(t_infected, tstep, days_in_step = 7,
+                         generation_fun) {
+
+  return(floor(t_infected + generation_fun(n)/days_in_step))
+
+}
+
+# get row id from cell id
+# (in simulation so that you don't have to have a super large matrix with empty cells!)
+rowfromcell <- function(cell_id, cells, rows) {
+  rows[match(cell_id, cells)]
+}
--- R/transmission.R
+++ R/transmission.R
@@ -23,15 +23,15 @@
 #' sim_trans(cell_id = cell_id, max_cells = max_cells,
 #'           S = S, N = N, E, I, V, track = TRUE)
 
-sim_trans <- function(cell_id, S, N, max_cells, track = FALSE, ...) {
+sim_trans <- function(row_id, S, N, max_cells, track = FALSE, ...) {
 
-  exps <- tabulate(cell_id, nbins = max_cells)
-  id_list <- unique(cell_id)
+  exps <- tabulate(row_id, nbins = max_cells)
+  id_list <- unique(row_id)
 
   if(track == FALSE) {
     # probability that a exposure was with a susceptible
     exps_out <- mapply(rbinom, n = 1, size = exps, prob = S/N)
-    outcome <- rep(0, length(cell_id))
+    outcome <- rep(0, length(row_id))
     contact <- NULL
 
     # Number of successful exposures can't be greater than the susceptibles available
@@ -41,7 +41,7 @@ sim_trans <- function(cell_id, S, N, max_cells, track = FALSE, ...) {
 
     for(i in seq_along(id_list)) {
       if(success[i] > 0) {
-        outcome[sample(which(cell_id == id_list[i]),
+        outcome[sample(which(row_id == id_list[i]),
                        success[i],
                        replace = FALSE)] <- 1
       }
@@ -49,15 +49,15 @@ sim_trans <- function(cell_id, S, N, max_cells, track = FALSE, ...) {
   } else {
 
     # track who exposure was allocated to
-    contact <- rep("", length(cell_id))
-    outcome <- rep(0, length(cell_id))
+    contact <- rep("", length(row_id))
+    outcome <- rep(0, length(row_id))
 
     for(i in seq_along(id_list)) {
       # Sample the states
       ind <- id_list[i]
       states <- rep(c("S", "E", "I", "V"), # taking out the infectious dog doing the biting (I[i] - 1)
                     c(S[ind], E[ind], I[ind] - 1, V[ind]))
-      contact[which(cell_id == ind)] <- sample(states, size = exps[ind],
+      contact[which(row_id == ind)] <- sample(states, size = exps[ind],
                                                replace = FALSE)
       outcome[which(contact == "S")] <- 1
     }
@@ -65,7 +65,6 @@ sim_trans <- function(cell_id, S, N, max_cells, track = FALSE, ...) {
 
   return(list(outcome = outcome, contact = contact))
 }
-
 # Notes :
 # only run this if length(cell_id > 0)
 # tests if everything is zero!
@@ -76,143 +75,136 @@ sim_trans <- function(cell_id, S, N, max_cells, track = FALSE, ...) {
 
 #' Simulate biting and movement
 #'
-#' \code{sim_bites} simulates bites and movement on landscape
-#'
-#' Number of secondary cases is drawn from a _dim, res_m,
-#'
+#' @param secondaries
+#' @param ids
+#' @param dispersal_fun
+#' @param counter
+#' @param cells_pop
+#' @param x_topl
+#' @param y_topl
+#' @param tstep
+#' @param sequential
+#' @param allow_empties
+#' @param leave_district
+#' @param max_tries
+#' @param ...
+sim_bites <- function(secondaries, ids, dispersal_fun, counter, res_m,
+                      cells_pop, nrow, ncol, x_topl, y_topl, tstep = t,
+                      sequential = TRUE, allow_empties = TRUE,
+                      leave_district = TRUE, max_tries = 100,
+                      ...) {
 
-
-sim_bites <- function(secondaries, ids, dispersal_fun, counter,
-                      cells_pop, x_topl, y_topl, tstep = t,
-                      sequential = TRUE, ...) {
-
-  nmoves <- sum(secondaries)
   progen_ids <- rep(ids, secondaries)
-  second_ids <- counter + 1:length(progen_ids)
-
   origin_x <- rep(x_coord, secondaries)
   origin_y <- rep(y_coord, secondaries)
-
-  distance <- dispersal_fun(nmoves) # length progen_ids
-  angle <- runif(n = nmoves, min = 0, max = 2*pi)
-
-  # Convert to a vector and move
-  x_moves <- (sin(angle) * distance * 1000)
-  y_moves <- (cos(angle) * distance * 1000)
 
   if(sequential == TRUE) {
 
     # first movement index of each progenitor
     inds <- mapply(function(x) which(progen_ids == x)[1], x = ids)
-    x_new <- y_new <- vector("numeric", length(progen_ids))
+    out <- vector("list", length(progen_ids))
 
     for (i in seq_along(progen_ids)) {
-
       if (i %in% inds) { # need progenitor coords for 1st movement
         x <- origin_x[i]
         y <- origin_y[i]
       } else {
-        x <- last_coords[1]
-        y <- last_coords[2]
+        x <- out$x_new[i - 1]
+        y <- out$y_new[i - 1]
       }
 
-      # Convert to a vector and move
-      x_new[i] <- (sin(angle[i]) * distance[i] * 1000) + x # convert to m
-      y_new[i] <- (cos(angle[i]) * distance[i] * 1000) + y
+      accept <- 0
+      tries <- 0
+      while (accept == 0 & tries < max_tries) {
 
-      # This means can go anywhere
-      col <- ceiling((x_new - x_topl)/res_m)
-      row <- ceiling(-(y_new - y_topl)/res_m)
-      cell <- ifelse(row < = row*ncol - (ncol - col)
+        move <- sim_movement(angle = runif(n = 1, min = 0, max = 2*pi),
+                            distance = dispersal_fun(1),
+                            x0 = x, y0 = y, x_topl,
+                            y_topl, res_m, ncol,
+                            nrow, cells_pop)
+        accept <- accept(leave_district, allow_empties, move$within,
+                         move$populated)
 
-      within <- ifelse(cell %in% cells_pop, 1, 0)
-      last_coords <- c(x_new[i], y_new[i])
-
+        tries <- tries + 1
+      }
+      out[i] <- move
     }
+
+    out <- rbindlist(out)
+
   } else {
+
+    nmoves <- sum(secondaries)
+
     # get origin from infector if kernel movements
-    x_new <- x_moves + origin_x # convert to m
-    y_new <- y_moves + origin_y
+    out <- sim_movement(angle = runif(n = nmoves, min = 0, max = 2*pi),
+                        distance = dispersal_fun(nmoves),
+                        x0 = origin_x, y0 = origin_y, x_topl,
+                        y_topl, res_m, ncol,
+                        nrow, cells_pop)
+
+    accept <- accept(leave_district, allow_empties, within = out$within,
+                     populated = out$populated)
+
+    tries <- 0
+    while(sum(accept == 0) > 0 & tries < max_tries) {
+
+      out[accept == 0, ] <-
+        sim_movement(angle = runif(n = sum(accept == 0), min = 0, max = 2*pi),
+                     distance = dispersal_fun(sum(accept == 0)),
+                     x0 = origin_x[accept == 0], y0 = origin_y[accept == 0],
+                     x_topl, y_topl, res_m, ncol, nrow, cells_pop)
+
+      accept <- accept(leave_district, allow_empties, within = out$within,
+                       populated = out$populated)
+
+      tries <- tries + 1
+    }
   }
 
+  # add in ids
+  out$id <- counter + 1:length(progen_ids)
+  out$progen_id <- progen_ids
+
+  return(out)
+}
+
+# above should inherit params x_topl, y_topl, res_m, ncol, nrow, cells_pop!
+# populated & within are boolean
+sim_movement <- function(angle, distance, x0, y0, x_topl,
+                         y_topl, res_m, ncol, nrow, cells_pop) {
+
+  x_new <- (sin(angle) * distance * 1000) + x0 # convert to m
+  y_new <- (cos(angle) * distance * 1000) + y0
+
+  # This means can go anywhere
+  col <- ceiling((x_new - x_topl)/res_m)
+  row <- ceiling(-(y_new - y_topl)/res_m)
+  cell <- row*ncol - (ncol - col)
+  populated <- cell %in% cells_pop
+  within <- row > 0 & row <= nrow & col > 0 & col <= ncol
+
+  return(data.table(x_new = x_new, y_new = y_new, cell = cell,
+                    populated = populated, within = within))
+
+}
+
+# inherit params: leave_district allow_empties
+accept <- function(leave_district, allow_empties,
+                   within, populated) {
+
+  accept <- rep(1, length(within))
+
+  if(leave_district == FALSE) {
+    accept <- ifelse(within, 1, 0)
+  }
+
+  if(allow_empties == FALSE) {
+    accept <- ifelse(populated, 1, 0)
+  }
+
+  return(accept)
 }
 
 
 
-  sim_trans_constrained <- function()
-
-    if(sequential == FALSE) {
-
-      # Do vectorized
-      # get origin from infector if kernel movements
-      x_new <- x_moves + origin_x # convert to m
-      y_new <- y_moves + origin_y
-
-      ## This means can go anywhere including outside of district
-      col <- ceiling((x_new - x_topl)/res_m)
-      row <- ceiling(-(y_new - y_topl)/res_m)
-      cell <- ifelse(row %in% 1:nrow(grid) & col %in% 1:ncol(grid), grid[row, col], 0)
-      ## This bit means only in populated places and doggies don't leave the district
-
-      within <- ifelse(cell %in% cells_pop, 1, 0)
-
-      # Repeat for ones not in district or which went to an empty patch
-      if(leave_district == FALSE) {
-
-      }
-
-      if(allow_empties = FALSE) {
-
-      }
-
-
-    }
-
-  }
-
-  store_coords_all <- vector("list", length(secondaries))
-
-  for (i in 1:length(secondaries)) {
-    if (secondaries[i] > 0) {
-
-      store_coords <- vector("list", secondaries[i])
-
-      for (j in 1:secondaries[i]){
-        counter <- counter + 1
-        if (j == 1) { # need progenitor coords for 1st movement
-          origin_x <- x_coord[i]
-          origin_y <- y_coord[i]
-        } else {
-          origin_x <- last_coords[1]
-          origin_y <- last_coords[2]
-        }
-        within <- 0
-        while (within == 0) {
-          distance <- dispersal_fun(1, shape = dispersalShape,
-                               scale = dispersalScale) # Distance to move in km
-          angle <- runif(n = 1, min = 0, max = 2*pi)  # Angle to move at
-
-          # Convert to a vector and move
-          x_new <- (sin(angle) * distance * 1000) + origin_x # convert to m
-          y_new <- (cos(angle) * distance * 1000) + origin_y
-
-          ## This means can go anywhere including outside of district
-          col <- ceiling((x_new - x_topl)/res_m)
-          row <- ceiling(-(y_new - y_topl)/res_m)
-          cell <- ifelse(row %in% 1:nrow(grid) & col %in% 1:ncol(grid), grid[row, col], 0)
-          ## This bit means only in populated places and doggies don't leave the district
-
-          within <- ifelse(cell %in% cells_pop, 1, 0)
-        }
-
-        store_coords[[j]] <- list(ID = counter, tstep = tstep, x_coord = x_new, y_coord = y_new,
-                                  progen_ID = ids[i], path_ID = j, cell_id = cell, sus = NA,
-                                  trans = NA_integer_, infectious = 0L, secondaries = NA_integer_)
-        last_coords <- c(x_new, y_new)
-      }
-      store_coords_all[[i]] <- store_coords
-    }
-  }
-  E_coords_now <- rbindlist(lapply(store_coords_all, rbindlist))
-  return(list(E_coords_now = E_coords_now, counter = counter))
-}
