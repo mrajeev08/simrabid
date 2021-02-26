aa27c2e392b2e1c6a1b64fad835ea6764f20860d
Malavika Rajeev <mrajeev08@gmail.com>
2021-02-25 21:52:48 -0800

    first crack at default parameterization

--- R/parameterize.R
+++ R/parameterize.R
@@ -1,15 +1,21 @@
 # Parameter distributions
-# think about how flexible you want this to
-# all functions should have n & param list
-# pass parameters for each into param list in sim.IBM
-# if you're fitting these then you want to be able to pass params easily in
-# the args of the function (I think this works well? Just need unique names?)
-# use best fits from katie as defaults here!
-# if you want to change this then just need to change the param list & the functions accordingly
+# all functions should have two arguments: n & param list
 
 # secondary function (should take from param list (n & params))
-negbinom_constrained <- function(n, params = list(R0 = 1.2, k = 1),
-                                 max_secondaries = 100) {
+#' Title
+#'
+#' @param n
+#' @param params
+#' @param max_secondaries
+#'
+#' @return
+#' @export
+#'
+#' @examples
+nbinom_constrained <- function(n,
+                               params = list(R0 = 1.2, k = 1),
+                               max_secondaries = 100) {
+
   secondaries <- rnbinom(n, size = params$k, mu = params$R0)
 
   # return constrained!
@@ -17,67 +23,136 @@ negbinom_constrained <- function(n, params = list(R0 = 1.2, k = 1),
                 max_secondaries, secondaries))
 }
 
-# dispersal function
-# defaults to Townsend et al. 2013 plos ntds
-# units = km?!
-dispersal_gamma <- function(n, params = list(disp_shape = 0.8,
-                                         disp_scale = 1)) {
+#' Title
+#'
+#' @param n
+#' @param params
+#'
+#' @return
+#' @export
+#'
+#' @examples
 
-  rgamma(n, shape = params$disp_shape, scale = params$disp_scale)
+dispersal_lognorm <- function(n,
+                              paramsp) {
+
+  rlnorm(n, meanlog = params$meanlog, sdlog = params$sdlog)
 
 }
 
-# defaults to Townsend et al. 2013 plos ntds
-# could also use an empirical sample of generation times per rebecca
-gen_gamma <- function(n, params = list(inc_shape = 1.46, inc_scale = 16.1)) {
+# serial interval
+#' Title
+#'
+#' @param n
+#' @param params
+#'
+#' @return
+#' @export
+#'
+#' @examples
+serial_lognorm <- function(n,
+                           params = param_defaults$serial) {
 
-  rgamma(n, shape = params$inc_shape, scale = params$inc_scale)
+  rlnorm(n, meanlog = params$meanlog, sdlog = params$sdlog)
+
+}
+
+# serial interval
+#' Title
+#'
+#' @param n
+#' @param params
+#'
+#' @return
+#' @export
+#'
+#' @examples
+steps_weibull <- function(n,
+                          params = param_defaults$steps) {
+
+  rweibull(n, shape = params$shape, scale = params$scale)
 
 }
 
 # generation time to the decimal timestep
+#' Title
+#'
+#' @param n
+#' @param t_infected
+#' @param days_in_step
+#' @param serial_fun
+#'
+#' @return
+#' @export
+#'
+#' @examples
 t_infectious <- function(n, t_infected, days_in_step = 7,
-                         generation_fun) {
+                         serial_fun) {
 
-  return(t_infected + generation_fun(n)/days_in_step)
+  return(t_infected + serial_fun(n)/days_in_step)
 
 }
 
 # Reporting model
+#' Title
+#'
+#' @param I_dt
+#' @param params
+#'
+#' @return
+#' @export
+#'
+#' @examples
 binom_detect <- function(I_dt, params = list(detect_prob = 0.9)) {
-  I_dt[, reported := rbinom(.N, size = 1, prob = params$detect_prob)]
+  I_dt[, detected := rbinom(.N, size = 1, prob = params$detect_prob)]
 }
 
-# Reporting model
-binom_beta_detect <- function(I_dt,
+# Reporting model with beta binomial distribution
+#' Title
+#'
+#' @param I_dt
+#' @param params
+#'
+#' @return
+#' @export
+#'
+#' @examples
+beta_detect <- function(I_dt,
                               params = list(alpha = 80.1,
                                             beta = 8.9)) {
   probs <- rbeta(1, params$alpha, params$beta)
-  I_dt[, reported := rbinom(.N, size = 1, prob = probs)]
+  I_dt[, detected := rbinom(.N, size = 1, prob = probs)]
 }
 
-# Monthly probabilities
-binom_beta_detect_monthly <- function(I_dt, tmax,
+# Reporting model with beta binomial distribution monthly
+#' Title
+#'
+#' @param I_dt
+#' @param tmax
+#' @param params
+#'
+#' @return
+#' @export
+#'
+#' @examples
+beta_detect_monthly <- function(I_dt, tmax,
                                       params = list(alpha = 80.1, beta = 8.9)) {
   I_dt[, month := floor(tstep/4)][, detect_prob := rbeta(1, params$alpha,
                                                    params$beta), by = month]
-  I_dt[, reported := rbinom(.N, size = 1, prob = detect_prob)]
+  I_dt[, detected := rbinom(.N, size = 1, prob = detect_prob)]
 }
 
-# beta_pars from mean
-est_beta_pars <- function(mean, var) {
-  alpha <- ((1 - mean) / var - 1 / mean) * mean ^ 2
-  beta <- alpha * (1 / mean - 1)
-  return(params = list(alpha = alpha, beta = beta))
-}
 
-# Getting probability from rate ------------------------------------------------
-
-# ## example 1: turn annual waning rate of 0.33 to weekly prob
-# get.prob(rate = 0.33, step = 52)
-# ## example 2: turn annual birth rate of 0.45 to monthly prob
-# get.prob(rate = 0.45, step = 12)
-
+# Getting probability from rate
+#' Title
+#'
+#' @param rate
+#' @param step
+#'
+#' @return
+#' @export
+#'
+#' @examples
 get_prob <- function(rate, step) {
   converted <- (1 + rate)^(1/step) - 1
   return(1 - exp(-converted))
--- data-raw/scripts/process_data.R
+++ data-raw/scripts/process_data.R
@@ -12,30 +12,34 @@ library(janitor)
 library(magrittr)
 library(lubridate)
 select <- dplyr::select
-source("R/utils-wm.R")
-
-# get latest fun
-get_latest <- function(path, pattern) {
-  list.files(path, full.names = TRUE)[grep(pattern, list.files(path))][1]
-}
+source("R/utils-data.R")
 
 # Data -------------------------------------
-sd_shape <- st_read("data-raw/raw/SD_shape/Serengeti_villages UTM_region.shp") # shapefile
-sd_pops <- read_csv("data-raw/raw/SerengetiPop.csv") # district wide census (@ vill level)
+
+# shapefile
+sd_shape <-
+  st_read("data-raw/raw/SD_shape/SD_From_HHS/SD_Villages_2002_From_HHS_250m_Smoothed_UTM.shp")
+sd_shape <- st_transform(sd_shape, crs = CRS("+init=epsg:32736"))
+
+# district wide census (@ vill level)
+sd_pops <- read_csv("data-raw/raw/SerengetiPop.csv")
 sd_census <- read_csv(get_latest("data-raw/raw/wisemonkey", "Census"))
 case_dt <- read_csv(get_latest("data-raw/raw/wisemonkey", "Animal_Contact_Tracing"))
 vacc_dt <- read_csv(get_latest("data-raw/raw/wisemonkey", "Vaccination"))
 
 # Get populations by village (growth & hdr) ----
 sd_pops %>%
-  clean_names(replace = c("Village_2002" = "pop_names")) %>%
-  group_by(pop_names) %>%
+  clean_names() %>%
+  group_by(village_2002) %>%
   summarize(pop_2002 = sum(population_2002, na.rm = TRUE),
             pop_2012 = sum(population_2012, na.rm = TRUE),
             villcode = as.character(villcodes[1])) %>%
   mutate(growth = exp(log(pop_2012/pop_2002)/10),
          row_id = row_number()) %>%
-  left_join(clean_names(sd_shape), .) -> sd_shape_pop
+  left_join(clean_names(sd_shape), .,
+            by = c("vill_2002" = "village_2002")) -> sd_shapefile
+
+usethis::use_data(sd_shapefile, overwrite = TRUE)
 
 # Clean census data and out neccessary bits -----
 sd_census %>%
@@ -43,30 +47,11 @@ sd_census %>%
   select(village_2002, utm_easting, utm_northing,
          adults, children, adult_dogs = dogs_3_months,
          adult_dogs_vacc = vaccinated_dogs_3_months,
-         pups = pups_3_months, pups_vacc = vaccinated_pups_3_months) -> sd_census_clean
+         pups = pups_3_months, pups_vacc = vaccinated_pups_3_months,
+         date = actvitity_date) %>%
+  filter(!is.na(utm_easting), !is.na(utm_northing)) -> sd_census_data
 
-# Use fasterize to get village ids
-r <- raster(sd_shape_pop)
-res(r) <- 500 # res in meters (lets do it at 500 to get best idea of locs)
-sd_rast <- fasterize(sd_shape_pop, r, field = "row_id")
-
-# Make sure there are no NAs!
-sd_census_clean %<>%
-  filter(!is.na(utm_easting), !is.na(utm_northing)) %>%
-  mutate(villcode = get_ids(x_coord = utm_easting, y_coord = utm_northing,
-                             res_m = 500,
-                             x_topl = bbox(sd_rast)[1, "min"],
-                             y_topl = bbox(sd_rast)[2, "max"],
-                             ncol = ncol(sd_rast),
-                             nrow = nrow(sd_rast),
-                             rast = sd_rast,
-                             id_col = sd_shape_pop$villcode))
-# ggplot(sd_shape_pop) +
-#   geom_sf() +
-#   geom_point(data = sd_census_clean,
-#              aes(x = utm_easting, y = utm_northing, color = villcode),
-#              alpha = 0.5) +
-#   guides(color = "none")
+usethis::use_data(sd_census_data, overwrite = TRUE)
 
 # Case data ----
 case_dt %>%
@@ -75,7 +60,9 @@ case_dt %>%
          symptoms_started_known, symptoms_started,
          symptoms_started_accuracy)  %>%
   filter(suspect %in% "Yes", symptoms_started_known,
-         species %in% "Domestic dog") -> case_data_cleaned
+         species %in% "Domestic dog") -> sd_case_data
+
+usethis::use_data(sd_case_data, overwrite = TRUE)
 
 # Vacc data ----
 vacc_dt %>%
@@ -84,7 +71,7 @@ vacc_dt %>%
          doses, dogs_vaccinated) %>%
   # when dogs_vaccinated not specified assume 95% of doses go to dogs
   mutate(dogs_vaccinated = coalesce(dogs_vaccinated,
-                                         round(doses * 0.95))) -> vacc_dt_cleaned
+                                         round(doses * 0.95))) -> sd_vacc_data
 
 correct_names <- tribble(~village_2002, ~corrected,
                           "Mbirikili", "Bonchugu",
@@ -93,13 +80,29 @@ correct_names <- tribble(~village_2002, ~corrected,
                           "Natta Mbiso" , "Mbisso",
                           "Nyamisingisi" , "Nyamasingisi")
 
-vacc_dt_cleaned %<>%
+sd_vacc_data %<>%
   left_join(correct_names) %>%
   mutate(village_2002 = coalesce(corrected, village_2002),
         match_2002 = sd_pops$VILLCODES[match(village_2002,
                                               sd_pops$Village_2002)],
-         match_2012 = sd_pops$VILLCODES[match(village_2002,
+        match_2012 = sd_pops$VILLCODES[match(village_2002,
                                                sd_pops$Village_2012)],
-         villcode = coalesce(match_2002, match_2012)) %>%
+        villcode = coalesce(match_2002, match_2012)) %>%
   select(-contains(c("match", "corrected")))
+
+usethis::use_data(sd_vacc_data, overwrite = TRUE)
+
+# Parameterization defaults (per Mancy et al. 2021) -----
+disp <- read.csv("data-raw/raw/parameters_KH/DK_params.csv")
+steps <- read.csv("data-raw/raw/parameters_KH/steps.distribution.csv")
+serial <- read.csv("data-raw/raw/parameters_KH/SI_params.csv")
+
+param_defaults <-
+  list(
+    "steps" = list(shape = steps$shape, scale = steps$scale),
+    "disp" = list(meanlog = disp$DK_meanlog, sdlog = disp$DK_sdlog),
+    "serial" = list(meanlog = serial$SI_ml, sdlog = serial$SI_sdlog)
+    )
+
+usethis::use_data(param_defaults, overwrite = TRUE)
 
